a68f21ca0683ad333be24fcac64de1cec6c8a734
MarkMwago2 <markmwago@gmail.com>
2019-05-02 04:18:59 +0300

    added abstract class animals

--- src/main/java/Animals.java
+++ src/main/java/Animals.java
@@ -1,2 +1,13 @@
-public class Animals {
+public abstract class Animals {
+    public String name;
+    public int id;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -1,2 +1,24 @@
-public class DatabaseRule {
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+
+public class DatabaseRule extends ExternalResource {
+
+    @Override
+    protected void before() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "mark", "87654321");
+    }
+
+    @Override
+    protected void after() {
+        try(Connection con = DB.sql2o.open()) {
+            String deleteAnimalsQuery = "DELETE FROM animals *;";
+            String deleteEndangeredQuery = "DELETE FROM endangered_animals *;";
+            String deleteSightingsQuery = "DELETE FROM sightings *;";
+            con.createQuery(deleteAnimalsQuery).executeUpdate();
+            con.createQuery(deleteEndangeredQuery).executeUpdate();
+            con.createQuery(deleteSightingsQuery).executeUpdate();
+        }
+    }
+
 }
